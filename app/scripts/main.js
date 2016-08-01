0e370b7babf6bc459330f3d54929d0e7f8d8142b
kamilazabik <kamilazabik@gmail.com>
2016-08-01 15:36:39 +0200

    sidebar różne wersje

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -32,19 +32,19 @@ $(document).ready(function(){
   $('#claim-hseq').click(function(e) {
     e.preventDefault();
     $('#content').load('./jade/projects.html');
-    $('#main-panel').addClass("fixed");
+    $('#main-panel').addClass('fixed');
   });
 
   $('#claim-hseq-3').click(function(e) {
     e.preventDefault();
     $('#content').load('./jade/panel-content.html');
-    $('#main-panel').addClass("fixed");
+    $('#main-panel').addClass('fixed');
   });
 
   $('#claim-hseq-4').click(function(e) {
     e.preventDefault();
     $('#content').load('./jade/panel-content-1.html');
-    $('#main-panel').addClass("fixed");
+    $('#main-panel').addClass('fixed');
   });
 
 
@@ -60,7 +60,7 @@ $(document).ready(function(){
     });
 
   $('.panel-fixed, .claim').click(function() {
-    $('#main-panel').addClass("fixed");
+    $('#main-panel').addClass('fixed');
     // console.log($('.panel-fixed'));
   });
 
@@ -97,18 +97,18 @@ $.fn.extend({
       tree.find('li').each(function() {
         var stick = $(this);
       });
-      tree.find('li').has("ul").each(function () {
+      tree.find('li').has('ul').each(function () {
         var branch = $(this); //li with children ul
         // console.log(branch);
 
-        branch.prepend("<i class='tree-indicator glyphicon glyphicon-chevron-right'></i>");
+        branch.prepend('<i class=\'tree-indicator glyphicon glyphicon-chevron-right\'></i>');
         branch.addClass('tree-branch');
         branch.on('click', function (e) {
           if (this == e.target) {
             var icon = $(this).children('i:first');
             console.log(icon);
 
-            icon.toggleClass("glyphicon-chevron-down glyphicon-chevron-right");
+            icon.toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
             $(this).children().children().toggle();
             // console.log($(this));
           }
@@ -136,12 +136,12 @@ $.fn.extend({
  *	The following snippet of code automatically converst
  *	any '.treeview' DOM elements into a treeview component.
  */
-$(window).on('load', function () {
-  $('.treeview').each(function () {
-    var tree = $(this);
-    tree.treeview();
-  })
-});
+// $(window).on('load', function () {
+//   $('.treeview').each(function () {
+//     var tree = $(this);
+//     tree.treeview();
+//   })
+// });
 
 
 
@@ -150,7 +150,7 @@ $(function () {
   $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
   $('.tree li.parent_li > span').on('click', function (e) {
     var children = $(this).parent('li.parent_li').find(' > ul > li');
-    if (children.is(":visible")) {
+    if (children.is(':visible')) {
       children.hide('fast');
       $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
     } else {
@@ -160,5 +160,23 @@ $(function () {
     e.stopPropagation();
   });
 });
+
+  // $(function () {
+  //   $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+  //   $('.tree li.parent_li > span').on('click', function (e) {
+  //     var children = $(this).parent('li.parent_li').find(' > ul > li');
+  //     if (children.hasClass("hidden")) {
+  //       children.removeClass('hidden')
+  //       children.show('fast');
+  //       $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+  //     } else {
+  //       children.addClass('hidden');
+  //       // children.hide('fast');
+  //       $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+  //
+  //     }
+  //     e.stopPropagation();
+  //   });
+  // });
 
 });
